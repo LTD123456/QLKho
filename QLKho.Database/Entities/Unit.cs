252799422ef7320e75a7d3ca44d79f4daa67fca5
Lê Thiện Đức <lethienduc123@gmail.com>
2020-05-13 15:21:58 +0700

    Create Entities

---
+++ QLKho.Database/Entities/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLKho.Database.Entities
+{
+    public class Unit
+    {
+        public string Id{ get; set; }
+        public string DisplayName { get; set; }
+        public bool Stop { get; set; }
+    }
+}
