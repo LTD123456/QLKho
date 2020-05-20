f5eab2b31a38cb3fbd86f2bb90e45e095f9f4646
Lê Thiện Đức <lethienduc123@gmail.com>
2020-05-20 17:45:19 +0700

    Create WebCore Layer

---
+++ QLKho.Service/Units/IUnits.cs
@@ -0,0 +1,20 @@
+﻿using QLKho.Data.Entities;
+using QLKho.Service.DTOCommons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLKho.Service.Units
+{
+    public interface IUnits
+    {
+        int Add(Unit unit);
+        int Update(Unit unit);
+        int UpdateRange(ICollection<Unit> listUnit);
+        //int AddOrUpdate()
+        int Delete(string Id);
+        ICollection<Unit> GetAll();
+        Unit GetByKey(string Key);
+        PageViewModel<Unit> GetAllPaging(string key = "", int pageIndex = 1, int pageSize = 50);
+    }
+}
